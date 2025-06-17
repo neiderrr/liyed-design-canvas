b1bac1807fee9826a97470013266e9f09f2f7830
neiderrr <168220048+neiderrr@users.noreply.github.com>
2025-06-17 10:30:03 -0500

    Create Anime.tsx

---
+++ src/pages/Anime.tsx
@@ -0,0 +1,10 @@
+const Anime = () => {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold">Categoría: Anime & Manga</h1>
+      <p className="text-lg mt-4">Aquí van las camisetas de anime y manga 🐉</p>
+    </div>
+  );
+};
+
+export default Anime;
